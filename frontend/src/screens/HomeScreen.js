8309005800ee0a0404053a5839d0ae0b5912face
hpy97 <yharshal255@gmail.com>
2025-01-06 23:07:19 +0530

    env variables added at backend & products API integrated at UI side

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { use, useEffect, useState } from 'react'
 import { Row, Col } from 'react-bootstrap';
-import products from '../products';
+import axios from 'axios';
 import Product from '../components/Product';
 
 
 function HomeScreen() {
+
+  const [products, setProducts] = useState([])
+
+    useEffect(() => {            
+        const fetchProducts = async () => {
+            const { data } = await axios.get('/api/products')
+            setProducts(data)
+        }
+        fetchProducts()
+    }, [])
+
   return (
     <>
         <h1>Latest Products</h1>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap';
 import Rating from '../components/Rating';
-
-import products from '../products';
+import axios from 'axios';
 
 const ProductScreen = () => {
+
+    const [product, setProduct] = useState({});
     const { id: productId } = useParams();
-    const product = products.find((product) => product._id === productId);
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${productId}`)
+            setProduct(data)
+        }
+        fetchProduct()
+    }, [productId])                            
 
   return (
     <>
