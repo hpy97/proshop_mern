349bd23fd837fef1ab93b4dfc1a01e655b6c4030
hpy97 <yharshal255@gmail.com>
2025-01-17 00:20:14 +0530

    redux toolkit setup & loader & message comp added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,33 +1,44 @@
-import React, { use, useEffect, useState } from 'react'
-import { Row, Col } from 'react-bootstrap';
-import axios from 'axios';
-import Product from '../components/Product';
-
+import React, { use, useEffect, useState } from "react";
+import { Row, Col } from "react-bootstrap";
+import axios from "axios";
+import Product from "../components/Product";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 function HomeScreen() {
+  // const [products, setProducts] = useState([])
 
-  const [products, setProducts] = useState([])
+  //   useEffect(() => {
+  //       const fetchProducts = async () => {
+  //           const { data } = await axios.get('/api/products')
+  //           setProducts(data)
+  //       }
+  //       fetchProducts()
+  //   }, [])
 
-    useEffect(() => {            
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products')
-            setProducts(data)
-        }
-        fetchProducts()
-    }, [])
+  const { data: products, error, isLoading } = useGetProductsQuery();
 
   return (
     <>
-        <h1>Latest Products</h1>
-        <Row>
-            {products.map((item,index) => (
-                <Col key={item._id} sm={12} md={6} lg={4} xl={3}>
-                    <Product product={item}/>
-                </Col>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant={'danger'}>{error?.data?.message || error.error}</Message>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((item, index) => (
+              <Col key={item._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={item} />
+              </Col>
             ))}
-        </Row>
+          </Row>
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
