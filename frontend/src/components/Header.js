d9c9f709923abd6274d1b669642d4b867e4a22bb
hpy97 <yharshal255@gmail.com>
2025-01-31 01:06:47 +0530

    login & logout functionality completed

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,44 +1,71 @@
 import React from "react";
-import { Badge, Navbar, Nav, Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { Badge, Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import { logout } from "../slices/authSlice";
 import logo from "../assets/styles/logo.png";
 
 const Header = () => {
-
   const { cartItems } = useSelector((state) => state.cart);
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+  const [logoutApiCall] = useLogoutMutation();
+  const navigate = useNavigate(); 
+
+  const logoutHandler = async () => {
+    try {
+      //made API call for logout
+      await logoutApiCall().unwrap();
+
+      //dispatch action to clear localstorage
+      dispatch(logout());
+
+      //redirct to login page
+      navigate('/login')
+    } catch (err) {
+      console.log(err)
+    }
+    
+  };
 
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          {/* <LinkContainer to={"/"}> */}
-            <Navbar.Brand href="/">
-              <img src={logo} alt="ProShop" />
-              ProShop
-            </Navbar.Brand>
-          {/* </LinkContainer> */}
+          <Navbar.Brand href="/">
+            <img src={logo} alt="ProShop" />
+            ProShop
+          </Navbar.Brand>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              {/* <LinkContainer to={"/cart"}> */}
-                <Nav.Link href="/cart">
-                  <FaShoppingCart /> Cart
-                  {
-                    cartItems.length > 0 && (
-                      <Badge pill bg="success" style={{ marginLeft: "5px" }}>
-                      {cartItems.reduce((acc, item) => acc + item.qty, 0)}
-                      </Badge>
-                    )
-                  }
-                </Nav.Link>
-              {/* </LinkContainer> */}
-              {/* <LinkContainer to={"/login"}> */}
+              <Nav.Link href="/cart">
+                <FaShoppingCart /> Cart
+                {cartItems.length > 0 && (
+                  <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                    {cartItems.reduce((acc, item) => acc + item.qty, 0)}
+                  </Badge>
+                )}
+              </Nav.Link>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  {/* <LinkContainer to={'/profile'}> */}
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                  {/* </LinkContainer> */}
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
                 <Nav.Link href="/login">
                   <FaUser /> Sign In
                 </Nav.Link>
-              {/* </LinkContainer> */}
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,15 +1,51 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
+import Loader from "../components/Loader";
+import { useLoginMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlice";
+import { toast } from "react-toastify";
 
 const LoginScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const submitHandler = (e) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [login, { isLoading }] = useLoginMutation();
+  const { userInfo } = useSelector((state) => state.auth);
+
+  // check url for redirection
+  const { search } = useLocation();
+  const searchParam = new URLSearchParams(search);
+  const redirect = searchParam.get('redirect') || '/';
+
+
+  useEffect(() => {
+    if(userInfo) {
+      navigate(redirect);
+    } 
+  },[userInfo, redirect])
+
+  const submitHandler = async (e) => {
     e.preventDefault();
-    console.log("submit", email,password);
+    try {
+
+      //made API call for login
+      const res = await login({email, password}).unwrap();
+
+      //dispatch action to save response in localstorage
+      dispatch(setCredentials({ ...res }));
+
+      //redirect to repsective path
+      navigate(redirect);
+
+    } catch(err) {
+      toast.error(err?.data?.message || err.error)
+    } 
+    
   };
 
   return (
@@ -34,15 +70,16 @@ const LoginScreen = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
-        <Button type="submit" variant="primary" className="mt-2">
+        <Button type="submit" variant="primary" className="mt-2" disabled={isLoading}>
           {" "}
           Sign In
         </Button>
+        { isLoading && <Loader/>}
       </Form>
 
       <Row className="py-3">
         <Col>
-          New Customer? <Link to="/register">Register Here</Link>
+          New Customer? <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>Register Here</Link>
         </Col>
       </Row>
     </FormContainer>
