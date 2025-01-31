a561c5056e4b3a70c6798521ec7faace1e2e4031
hpy97 <yharshal255@gmail.com>
2025-01-31 23:40:44 +0530

    registration done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,6 +16,7 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 //import reportWebVitals from './reportWebVitals';
 
 const router = createBrowserRouter(
@@ -25,6 +26,7 @@ const router = createBrowserRouter(
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
     </Route>
   )
 );
